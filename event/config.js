ccbfd3c5af91527fb80fbb75b50556e914a31d2d
Ajay <ajkush@gmail.com>
2020-08-02 14:24:05 +0200

    Update config.js

--- event/config.js
+++ event/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    kafka_topic: 'event-bus',
+    kafka_topic: 'event_bus',
     kafka_server_host: process.env.KAFKA_HOST || "zookeeper-service.kafka-ca1",
     kafka_server_port: process.env.ZOOKEEPER_PORT || "2181",
   };
